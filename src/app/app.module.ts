b16e5ea368a988f411ceab19071228d739895f1d
jburns6789 <117755326+jburns6789@users.noreply.github.com>
2023-12-18 11:11:15 -0500

    dockerfile addition, addtion order button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { ProductListComponent } from './components/product-list/product-list.com
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http'; // part of angular framework needs to be added
 import { ProductService } from './services/product.service'; //needs to be added
 
-import { Routes, Module, } from '@angular/';
+import { Routes, RouterModule, Router} from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
@@ -42,10 +42,10 @@ const oktaAuth = new OktaAuth(oktaConfig);
 
 function sendToLoginPage(oktaAuth: OktaAuth, injector: Injector) {
   // Use injector to access any service available withinn your application
-  const  = injector.get();
+  const router  = injector.get(Router);
 
   // Redirect the user to your custom login page
-  .navigate(['/login']);
+  router.navigate(['/login']);
 }
 
 // define and configure routes
@@ -92,7 +92,7 @@ const routes: Routes =[
     ServiceOfferingListComponent
   ],
   imports: [
-    Module.forRoot(routes), // routes from the const routes goes here
+    RouterModule.forRoot(routes), // routes from the const routes goes here
     BrowserModule,
     HttpClientModule, //needs to be added for api support
     NgbModule, // necessary to support ng bootstrap, exposes the exported delclarations
