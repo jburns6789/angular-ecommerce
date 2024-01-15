75a2e0478b8c86fcd9c80f52bc3fd3bf84f12a83
jburns6789 <117755326+jburns6789@users.noreply.github.com>
2024-01-15 09:54:31 -0500

    angular-testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,17 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-ecommerce');
   });
 
-  it('should render title', () => {
+  it('should render img alt tag luv2shop', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-ecommerce app is running!');
+    expect(compiled.querySelector('img.img-responsive')?.attributes?.getNamedItem("alt").value).toEqual("luv2shop");
+  });
+
+  it('should render img source equal assets/images/logo.png', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('img.img-responsive')?.attributes?.getNamedItem("src").value).toEqual("assets/images/logo.png");
   });
 });
--- src/app/services/order-history.service.spec.ts
+++ src/app/services/order-history.service.spec.ts
@@ -1,39 +1,33 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { OrderHistoryService } from './order-history.service';
 import { OrderHistory } from '../common/order-history';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { OrderHistoryComponent } from '../components/order-history/order-history.component';
 
-// describe('OrderHistoryService', () => {
-//   let service: OrderHistoryService;
-//   beforeEach(() => {
-//     //service = new OrderHistoryService();
-//   });
+describe('OrderHistoryService', () => {
+  let service: OrderHistoryService;
+  let testingController: HttpTestingController;
 
-  
   beforeEach(() => {
-    TestBed.configureTestingModule({providers: [OrderHistoryComponent, {provide:OrderHistoryService}]});
-    //service = TestBed.inject(OrderHistoryService);
+    TestBed.configureTestingModule({ imports:[
+      HttpClientModule, HttpClientTestingModule
+      ]
+    });
+    service = TestBed.inject(OrderHistoryService);
 
-
+    testingController = TestBed.inject(HttpTestingController);
   });
-  
-  const comp = new OrderHistoryComponent(OrderHistoryService);
-  comp=TestBed.inject(OrderHistoryComponent)
 
-  it('should deleteOrder', () => {
-    const deleteMethod = new delete(new OrderHistory)
-    //expect(service).toBeTruthy();
-
-    expect(result).toBeTruthy
+  it('should get customer', () => {
+    
   });
 
   it('should updateOrderQuantity', () => {
-    pending();
+    //pending();
     //expect(service).toBeTruthy();
   });
 
-  
+
 
 });
